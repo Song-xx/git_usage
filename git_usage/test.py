7f88c76825cfbc115669db9b9e3426658f9ee3f0
Song-xx <e18281828@163.com>
2021-09-30 10:06:00 +0800

    删除了一个文件夹

--- git_usage/test.py
+++
@@ -1,5 +0,0 @@
-print('this is a new file !')
-print('this is added online gitlab')
-print('this is dev-branch1 @18:00')
-print('this is master-branch1 @18:40')
-print('this is master-branch1 @20210929-10:16')
--- git_usage/test3.py
+++
@@ -1,2 +0,0 @@
-print('this is test3.py')
-'branch1-test3.py'
